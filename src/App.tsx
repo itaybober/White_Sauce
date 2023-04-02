c2ebc1a0b49252eadb308cef214ad255aebcd161
itayb <itay.bober@mail.huji.ac.il>
2023-04-02 18:54:24 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Achsaf Was Here
-        </p>
+        <h1>
+          Achsaf Was Here {'\n'} Itay Was Here
+        </h1>
+
         <a
           className="App-link"
           href="https://reactjs.org"
