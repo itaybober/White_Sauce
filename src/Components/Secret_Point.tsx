258a78d255a4076281145b638b4418532cc88392
gsfrank8 <127945201+gsfrank8@users.noreply.github.com>
2023-06-14 16:33:00 +0300

    הוספתי עמוד חדש בשם secret_point , אותו נכניס בסוף ויוכלו לעדכן אם הם הצליחו במשימה הסודית - משם עוברים לעמוד האחרון.

---
+++ src/Components/Secret_Point.tsx
@@ -0,0 +1,49 @@
+import Typography from '@mui/material/Typography';
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import * as React from "react";
+import { useState } from "react";
+import Background_loser from "../Components/Background-loser";
+import Avatar_and_points from "../Components/avatar_and_points";
+
+// @ts-ignore
+export default function Secret_Point({ curPlayer, curGame }) {
+    const [itemData, setItemData] = useState([]);
+
+    return (
+        <Container
+            className="punishment_page_component"
+            style={{ overflowX: 'hidden', height: '100%' }}
+        >
+            <Avatar_and_points name={curPlayer._name} points={curPlayer._points} />
+
+            <Typography
+                variant="h6"
+                gutterBottom
+                style={{ marginTop: '25%' }}
+            >
+                Did you succeed in your secret mission?
+            </Typography>
+
+            <div style={{ marginTop: '10%' }}>
+                <Button
+                    // onClick={() => curPlayer.setCurPage(PAGES.END)}
+                    variant="contained"
+                    color="secondary"
+                    size="large"
+                    sx={{ marginRight: '50px' }}
+                >
+                    No
+                </Button>
+                <Button
+                    // onClick={() => curPlayer.setCurPage(PAGES.END)}
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                >
+                    Yes
+                </Button>
+            </div>
+        </Container>
+    );
+}
