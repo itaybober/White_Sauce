dbc548b74fcf90d48d42c2e61ee7599cbb7daaee
maya <maya.staruss@mail.huji.ac.il>
2023-06-18 19:21:54 +0300

    Secret mission update

--- src/Pages/Secret_Mission.tsx
+++ src/Pages/Secret_Mission.tsx
@@ -8,7 +8,8 @@ import "./Punishment_Page.css"
 import Avatar_and_points from "../Components/avatar_and_points";
 import Flippable_card from "../Components/Flippable_card";
 import {PAGES} from "./GameManager";
-import a from './images/icon/a.png'
+import secret from './images/cards icons/card11.png'
+// import survivel2 from "./images/cards icons/card14.png";
 
 // @ts-ignore
 export default function Secret_Mission({curPlayer,curGame}) {
@@ -25,32 +26,33 @@ export default function Secret_Mission({curPlayer,curGame}) {
             <Avatar_and_points name={curPlayer._name} points={curPlayer._points} />
             <Flippable_card back_content={
                 <div>
-                    <CardContent sx={{display: "flex", flexFlow:"column", justifyContent: "flex-start", alignItems: "flex-start" ,textAlign: "justify"}}>
-                        <Typography variant="h5" color={"secondary"}> <b> {mission_object._title}</b></Typography>
-                        <Typography  sx={{lineHeight: '20px'}} variant={"h6"} >
-                            {mission_object._description}
-                        </Typography>
-                        <Typography>
-
-                        </Typography>
+                    <CardContent sx={{display: "flex", flexFlow:"column", justifyContent: "flex-start", alignItems: "flex-start" ,textAlign: "justify" }}>
+                        <Typography variant="h5" color={"primary"}> <b> {mission_object._title}</b></Typography>
+                        <br/>
+                        <Typography variant="h6">
+                            {mission_object._description}   </Typography>
                     </CardContent>
                 </div>
 
             }
-                            front_content={
-                                <div>
-                                    <CardContent sx={{ display: "flex", flexFlow: "column", justifyContent: "center"}}>
-                                        <Typography variant={"h4"} >Secret Mission</Typography>
-                                        <div style={{ maxWidth: '100%', maxHeight: '100%', overflow: 'hidden' }}>
-                                            <img src={a} alt="a" style={{ maxWidth: '30%', maxHeight: '30%', objectFit: 'contain' }} />
-                                        </div>
-                                        <div style={{ marginTop: "auto" }}>
-                                            <Typography variant={"h6"} sx={{ textAlign: "center" }}>Make sure no one is peeking, then click to reveal your secret mission</Typography>
-                                        </div>
-                                    </CardContent>
-                                </div>
-                            }
-            />
+                front_content={
+                    <div>
+                        <CardContent sx={{display: "flex", flexFlow:"column", justifyContent: "center", alignItems: "center" ,textAlign: "center" }}>
+                            <img src={secret} style={{ width: 180, height: 200, opacity: 0.7 }} />
+                            <br/>
+                            <Typography variant={"h4"}> <b>Secret Mission</b></Typography>
+                            <Typography variant={"h6"}> Make sure no one is peeking, then click to reveal your secret mission</Typography>
+                            {/*<Typography variant={"h4"} >Secret Mission</Typography>*/}
+                            {/*<div style={{ maxWidth: '100%', maxHeight: '100%', overflow: 'hidden' }}>*/}
+                            {/*    <img src={secret} alt="a" style={{ maxWidth: '30%', maxHeight: '30%', objectFit: 'contain' }} />*/}
+                            {/*</div>*/}
+                            {/*/!*<div style={{ marginTop: "auto" }}>*!/*/}
+                            {/*    <Typography variant={"h6"} sx={{ textAlign: "center" }}></Typography>*/}
+                            {/*</div>*/}
+                        </CardContent>
+                    </div>
+                }
+        />
 
             <Button onClick={()=> curPlayer.setCurPage(PAGES.SURV)} variant="contained" color="primary" size={"medium"} sx={{
                 mb: 4,
