a43d85bd230c73b604606b81591872a9b23d0f8e
Achsaf Atzmon <achsaf6@gmail.com>
2023-06-18 19:10:26 +0300

    nothing

--- src/Pages/GameManager.tsx
+++ src/Pages/GameManager.tsx
@@ -146,6 +146,7 @@ function GameManager() {
                             Player.addPlayerToFirestore(curUser);
                             if (curPlayer){
                                 setPage(curPlayer._curPage)
+
                             }
                             console.log("Creating new player: " + curUser._name);
                         }
