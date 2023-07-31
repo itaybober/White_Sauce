5e77e672b6fb59aff923da2ece27b8a41446cc8a
gsfrank8 <127945201+gsfrank8@users.noreply.github.com>
2023-07-31 16:58:16 +0300

    שיניתי את הgroup mission והוספץי אופתציה להזין האם הצלחתי במשימה הסודית

--- src/Pages/GroupMission.tsx
+++ src/Pages/GroupMission.tsx
@@ -3,80 +3,105 @@ import Typography from '@mui/material/Typography';
 import Button from "@mui/material/Button";
 import CardContent from '@mui/material/CardContent';
 import Avatar_and_points from "../Components/avatar_and_points";
-import "./Survival_mission.css"
-import {useState} from "react";
-import {Container} from "@mui/material";
+import { useState } from "react";
+import { Container } from "@mui/material";
 import friends from "./Souvenirs/tangled.jpg"
-import "./GroupMission.css"
 import Flippable_card from "../Components/Flippable_card";
-import {PAGES} from "./GameManager";
+import { PAGES } from "./GameManager";
 import CameraComponent from "../Components/CameraComponent";
 import shield from '../Pages/images/cards icons/card13.png'
 
-// we need to add the stepper here later
-// function  winner_list_update({name,points,bg}){
-//     return(
-//     <Winner_list name1={"Achsaf"} points1={340} bg1={"#D9FB68"} name2={"Itay"} points2={240} bg2={"#EFB2B2"} name3={name} points3={points} bg3={bg} />
-//     )
-//
-// }
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+
 // @ts-ignore
-
-export default function GroupMission({curPlayer,curGame}) {
-
-    const mission_object = curGame._curMission
-
+export default function GroupMission({ curPlayer, curGame }) {
+    const mission_object = curGame._curMission;
     const [itemData, setItemData] = useState([]);
+    const [showMissionCard, setShowMissionCard] = useState(false); // State to control the card visibility
     const [isPictureUploaded, setIsPictureUploaded] = useState(false);
 
     const handlePictureUpload = ()=> {
         setIsPictureUploaded(true);
     }
-
-
     function addPhoto() {
         // @ts-ignore
-        setItemData([friends])
+        setItemData([friends]);
     }
+
     return (
-            <Container className={"group_mission_component"} sx={{p:2}}>
-                <Avatar_and_points name={curPlayer._name} points={curPlayer._points} />
-                <Flippable_card
-                    back_content={
-                    <CardContent sx={{display: "flex", flexFlow:"column", justifyContent: "flex-start", alignItems: "flex-start" ,textAlign: "justify"}}>
+        <Container className={"group_mission_component"} sx={{ p: 2 }}>
+            <Avatar_and_points name={curPlayer._name} points={curPlayer._points} />
+            <Flippable_card
+                back_content={
+                    <CardContent sx={{ display: "flex", flexFlow: "column", justifyContent: "flex-start", alignItems: "flex-start", textAlign: "justify" }}>
                         <Typography variant="h5" color={"primary"}>{mission_object._title}</Typography>
-                    <br/>
+                        <br />
                         <Typography variant="h6">
                             {mission_object._description}
                         </Typography>
                     </CardContent>
                 }
-
                 front_content={
                     <div>
-                        <CardContent sx={{display: "flex", flexFlow:"column", justifyContent: "center", alignItems: "center" ,textAlign: "center" }}>
-                            <img src={shield} style={{ width: 200, height: 200, opacity: 0.7 }}/>
-                            <br/>
+                        <CardContent sx={{ display: "flex", flexFlow: "column", justifyContent: "center", alignItems: "center", textAlign: "center" }}>
+                            <img src={shield} style={{ width: 200, height: 200, opacity: 0.7 }} />
+                            <br />
                             <Typography variant={"h4"}> <b>tribe mission</b></Typography>
                             <Typography variant={"h5"}> {mission_object._title}</Typography>
                         </CardContent>
                     </div>
-                                }
+                }
+            ></Flippable_card>
 
+            <CameraComponent buttonText="Proof Of Concept" onPictureUpload={handlePictureUpload}/>
 
-                ></Flippable_card>
-
-
-                <CameraComponent buttonText="Proof Of Concept" onPictureUpload={handlePictureUpload}/>
-
-                <Button onClick={()=> {
-                    curGame.updateAllPlayersPages(PAGES.END)
-                }} variant="contained" color="primary" size={"medium"} sx={{
-                    mb: 4,
-
-                }} >Next</Button>
-            </Container>
+            {showMissionCard ? (
+                <Card sx={{ mb: 4 }}>
+                    <CardContent>
+                        <Typography variant="h6">
+                            Did you succeed in the secret mission?
+                        </Typography>
+                    </CardContent>
+                    <CardActions sx={{ justifyContent: 'center' }}>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            size="medium"
+                            onClick={ () => {
+                                console.log("clicked")
+                                curGame.updateAllPlayersPages(PAGES.END); // Transition to the next page
+                            }}
+                        >
+                            Yes
+                        </Button>
+                        <Button
+                            variant="contained"
+                            color="secondary"
+                            size="medium"
+                            onClick={() => {
+                                curGame.updateAllPlayersPages(PAGES.END); // Transition to the next page
+                            }}
+                        >
+                            No
+                        </Button>
+                    </CardActions>
+                </Card>
+            ) : (
+                <Button
+                    onClick={() => {
+                        setShowMissionCard(true); // Show the mission card when the "Next" button is clicked
+                    }}
+                    variant="contained"
+                    color="primary"
+                    size="medium"
+                    sx={{
+                        mb: 4,
+                    }}
+                >
+                    Next
+                </Button>
+            )}
+        </Container>
     );
-
 }
-
