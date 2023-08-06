06826d20c74f0a06bc48129ccfbb4378d0c9715c
maya <maya.staruss@mail.huji.ac.il>
2023-08-06 12:05:30 +0300

    avatar_and_points.tsx

--- src/Components/avatar_and_points.tsx
+++ src/Components/avatar_and_points.tsx
@@ -7,10 +7,11 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Avatar from "@mui/material/Avatar";
 import Container from "@mui/material/Container";
-import mask from '../Pages/images/icon/avatar.png'
+import mask from '../Pages/images/icon/avatar.png'import coin from "../Pages/images/cards icons/shield.png"
 
 import logo from "../Pages/images/step-1_logo.svg";
 import {useEffect} from "react";
+import survivel2 from "../Pages/images/cards icons/card14.png";
 
 
 // @ts-ignore
@@ -28,38 +29,41 @@ export default function Avatar_and_points({name = "maya", points, avatarName, av
 
 
         <Container sx={{
-            width: 330,
-            height: 120,
+            width: 310,
+            // height: 120,
             display: 'flex',
             flexDirection: 'row',
-            justifyContent: "space-between",
+            justifyContent: "center",
             alignItems: 'center'
         }}>
             <Avatar alt={mask} src={avatarRef} sx={{width: 60, height: 80}}> </Avatar>
 
             <Container sx={{
-                width: 240,
+                width: 250,
                 height: 80,
                 display: 'flex',
                 flexDirection: 'column',
-                justifyContent: "space-between",
-                alignItems: 'flex-start',
+                justifyContent: "flex-start",
+                alignItems:  "flex-start",
                 gap: "10px"
             }}>
                 <Typography
                     variant="h5"> {name} </Typography>
-                <Card sx={{width: 235, height: 45}}>
+                <Card sx={{width: 240, height: 45, borderRadius: '10px'}}>
                     <CardContent sx={{
                         display: 'flex',
                         flexDirection: 'row',
-                        justifyContent: 'space-around',
+                        justifyContent: 'space-between',
                         alignItems: 'center',
                         height: 45
                     }}>
+
+
                         <Typography
-                            variant="h6"> {points} points </Typography>
-                        <Typography color={"primary"}
-                                    variant="h6"> #2 </Typography>
+                            variant="h6">{points} coins </Typography>
+
+                        <img src={coin} style={{ width: 20, height: 20 }} />
+
                     </CardContent>
                 </Card>
             </Container>
